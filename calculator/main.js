1b34f1412a36e1948b8c45ca6eb1b11e4dc7481d
Bhavesh299 <97518972+Bhavesh299@users.noreply.github.com>
2022-03-02 22:51:04 +0530

    Assignment 1
    Done

---
+++ calculator/main.js
@@ -0,0 +1,9 @@
+const add = require("./add");
+const sub = require("./substract");
+const multiply = require("./multiply");
+const devide = require("./divide");
+
+add() ;
+sub() ;
+multiply() ;
+devide() ;
